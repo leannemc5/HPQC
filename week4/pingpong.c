2dc8e56dfc8f6c23072dd8638b90922386fcc877
leannemc5 <leanne.mcmanus2@mail.dcu.ie>
2026-04-18 17:18:33 +0100

    Created pingpong program from pseudocode

--- week4/pingpong.c
+++ week4/pingpong.c
@@ -1,3 +1,4 @@
+// needed libraries
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
@@ -10,23 +11,24 @@ int main(int argc, char **argv)
     int ierror = 0;
     int my_rank, uni_size;
 
-    // 1. Check arguments before MPI Init
+    // check arguments before MPI Init
     int num_pings = check_args(argc, argv);
-
+    // initialsie mpi enviroment
     MPI_Init(&argc, &argv);
+    // get rank and total numbere of process
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &uni_size);
 
-    // 2. Safety check: This benchmark requires exactly 2 processes
+    // ensure only 2 processes are used
     if (uni_size != 2)
     {
         if (my_rank == 0) 
             fprintf(stderr, "Error: Ping-pong requires exactly 2 processes.\n");
-        MPI_Finalize();
+        MPI_Finalize(); // clean up mpi enviroment
         return -1;
     }
 
-    // 3. Run the benchmark
+    // run the benchmark
     run_pingpong(my_rank, num_pings);
 
     MPI_Finalize();
@@ -35,39 +37,40 @@ int main(int argc, char **argv)
 
 void run_pingpong(int my_rank, int num_pings)
 {
-    int counter = 0;
-    int tag = 0;
-    int count = 1;
+    int counter = 0; // counter passed between processes
+    int tag = 0;  // message tag to match send/receives
+    int count = 1; // number of elements to send
     double start_time, end_time;
     MPI_Status status;
 
-    // Synchronize processes before starting the timer
+    // synchronize processes before starting the timer
     MPI_Barrier(MPI_COMM_WORLD);
+    // record start time
     start_time = MPI_Wtime();
-
+    // loop until counter reaches desired numbr of pings
     while (counter < num_pings)
     {
         if (my_rank == 0)
         {
-            // Root sends (Ping)
+            // Root sends (ping) to process
             MPI_Send(&counter, count, MPI_INT, 1, tag, MPI_COMM_WORLD);
-            // Root receives (Pong)
+            // Root receives (pong)
             MPI_Recv(&counter, count, MPI_INT, 1, tag, MPI_COMM_WORLD, &status);
         }
         else if (my_rank == 1)
         {
-            // Client receives (Ping)
+            // client receives (ping)
             MPI_Recv(&counter, count, MPI_INT, 0, tag, MPI_COMM_WORLD, &status);
-            // Client increments
+            // client increments
             counter++;
-            // Client sends (Pong)
+            // client sends (pong)
             MPI_Send(&counter, count, MPI_INT, 0, tag, MPI_COMM_WORLD);
         }
     }
-
+    // record end time
     end_time = MPI_Wtime();
 
-    // 4. Output results from Root
+    // output results from Root
     if (my_rank == 0)
     {
         double elapsed_time = end_time - start_time;
@@ -79,13 +82,15 @@ void run_pingpong(int my_rank, int num_pings)
         printf("Average Time per Round-Trip: %f s\n", avg_time);
     }
 }
-
+// validate coomand line arguments
 int check_args(int argc, char **argv)
 {
+    //expects only 1 argument, 1 ping
     if (argc != 2)
     {
         fprintf(stderr, "Usage: mpirun -np 2 pingpong [NUM_PINGS]\n");
         exit(-1);
     }
+    // convert argument to integer and return it
     return atoi(argv[1]);
 }
